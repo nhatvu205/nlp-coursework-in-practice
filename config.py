9192a5515c0d5dc63ab14f6759ed12e2af6ec2d3
Nhat Vu <23521104@gm.uit.edu.vn>
2026-01-05 21:29:46 +0700

    add modules

---
+++ config.py
@@ -0,0 +1,32 @@
+class Config:
+    models = {
+        'mbert': 'bert-base-multilingual-cased',
+        'xlmr': 'xlm-roberta-base',
+        'phobert': 'vinai/phobert-base'
+    }
+    
+    models_to_train = ['mbert', 'xlmr', 'phobert']
+    
+    data_path = '/kaggle/working'
+    train_dir = 'train'
+    dev_dir = 'dev'
+    test_dir = 'test'
+    
+    output_dir = '/kaggle/working/output'
+    
+    max_length = 384
+    doc_stride = 128
+    max_answer_length = 30
+    
+    batch_size = 16
+    learning_rate = 3e-5
+    num_epochs = 3
+    warmup_steps = 500
+    weight_decay = 0.01
+    
+    early_stopping_patience = 3
+    early_stopping_threshold = 0.001
+    
+    seed = 42
+    fp16 = True
+
